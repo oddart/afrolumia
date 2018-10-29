85aed239834461d7eac6387df93b5a3666d79a1a
Usheninte <jrandangana@gmail.com>
2018-10-29 09:27:20 +0100

    editions

--- src/components/views/intro.js
+++ src/components/views/intro.js
@@ -4,12 +4,16 @@ const under = {
   textDecoration: "underline"
 }
 
+const honey = {
+  color: "#eb9605"
+}
+
 export default class Intro extends Component {
   render() {
     return(
       <div>
         <h1 style={under}>A f r o L u m i a</h1>
-        <p>for bohemians with culture</p>
+        <p style={honey}>for bohemians with culture</p>
       </div>
     );
   }
