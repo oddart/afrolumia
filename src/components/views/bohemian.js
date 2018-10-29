03ce64f4b29e2c889f168de1388cdfcb15b8de57
Usheninte <jrandangana@gmail.com>
2018-10-29 20:58:00 +0100

    adding bohemian definition

--- src/components/views/bohemian.js
+++ src/components/views/bohemian.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+const note = {
+  color: "#000",
+  fontStyle: "italic"
+}
+
 export default class Bohemian extends Component {
   render() {
     return(
       <div>
-        <h4></h4>
+        <div id="card">
+          <h4 style={note}>/bəʊˈhiːmɪən/</h4>
+        </div>
       </div>
     );
   }
