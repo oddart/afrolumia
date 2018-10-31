0af68eb9076a7db0f6fc1677db9d5d44a5301c80
Usheninte <jrandangana@gmail.com>
2018-10-31 07:38:47 +0100

    added note

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,18 @@ const footerStyle = {
   marginTop: "4em"
 }
 
+const honeyBold_underWhite = {
+  color: "#eb9605",
+  fontStyle: "bold",
+  textDecoration: "underline",
+  textDecorationColor: "#fff"
+}
+
 export default class Footer extends Component {
   render() {
     return(
       <div style={footerStyle}>
+        <h5>a <span style={honeyBold_underWhite}>poetrique</span> initiative</h5>
         <h6>Usheninte &copy; 2018</h6>
       </div>
     );
