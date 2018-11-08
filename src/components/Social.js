8ddd2239d37792f2025c5b3f8e6fe11aa674a484
Usheninte <jrandangana@gmail.com>
2018-11-08 07:06:57 +0100

    added icon

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+const honey = {
+  color: "#eb9605",
+  marginTop: "4em"
+}
+
 export default class Social extends Component {
   render() {
     return(
       <div>
-        <a href="https://instagram.com" alt="Instagram" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-instagram fa-2x social"></i>
+        <a style={honey}
+        href="https://instagram.com" 
+        alt="Instagram" target="_blank" 
+        rel="noopener noreferrer">
+          <i class="fab fa-instagram fa-3x social"></i>
         </a>
       </div>
     );
