2df4f257bf17e1bd2ef38a71e1cb39015a1d3b52
Usheninte <jrandangana@gmail.com>
2018-11-08 07:11:41 +0100

    added icon msg

--- src/components/Social.js
+++ src/components/Social.js
@@ -11,12 +11,15 @@ export default class Social extends Component {
   render() {
     return(
       <div>
-        <a style={honey}
-        href="https://instagram.com" 
-        alt="Instagram" target="_blank" 
-        rel="noopener noreferrer">
-          <i class="fab fa-instagram fa-3x social"></i>
-        </a>
+        <div style={honey}>
+          <h4>Join the Movement</h4>
+          <a style={honey}
+          href="https://instagram.com" 
+          alt="Instagram" target="_blank" 
+          rel="noopener noreferrer">
+            <i class="fab fa-instagram fa-3x social"></i>
+          </a>
+        </div>
       </div>
     );
   }
